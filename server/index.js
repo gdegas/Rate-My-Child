541ade8d6daedd8d732923f8129e8c19abd12671
gdegas001 <gdegas001@gmail.com>
2017-07-11 15:43:35 -0700

    Used express as a file server with static middleware

--- server/index.js
+++ server/index.js
@@ -2,7 +2,12 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const insertStudent = require('./insert-student')
+const path = require('path')
 
+const publicPath = path.join(__dirname, 'rate-my-child')
+const staticMiddleware = express.static(publicPath)
+
+app.use(staticMiddleware)
 app.use(bodyParser.json())
 
 app.post('/students', (req, res) => {
