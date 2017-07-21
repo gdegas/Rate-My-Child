84a9d7585cf0db58663587c5032f6eb655f9dddf
gdegas001 <gdegas001@gmail.com>
2017-07-21 13:37:32 -0700

    defined a get method that uses knex to select all reports by student name.#11

--- server/crud-reports.js
+++ server/crud-reports.js
@@ -8,6 +8,14 @@ const reports = {
     const query = knex
       .insert(report)
       .into('reports')
+      .returning('*')
+    return query
+  },
+  get: (name) => {
+    const query = knex
+      .where('name', name)
+      .select('*')
+      .from('reports')
     return query
   }
 }
